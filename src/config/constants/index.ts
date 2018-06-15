cbaab42bc37b3bddf9bc4f66309cbc85f34410ae
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2018-06-14 23:25:44 -0300

    added repo url, author and wallet address in constants

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,11 +1,14 @@
 import { IConstants } from './types'
 
 const constants: IConstants = {
-    APP_NAME: 'DecredTickets',
+    APP_NAME: 'Decred Tickets',
+    AUTHOR: 'Armando Assunção',
+    DECRED_WALLET_ADDRESS: 'DsamJpphLB5rux2pDwqEw2FJHa4KuSXAnq1',
     HTTP_HEADERS: {
         ACCEPT: 'application/json',
     },
     REDUX_PERSIST_KEY: 'redux_persist_key',
+    REPO_URL: 'https://www.github.com/ArmandoAssuncao/decred_tickets',
 }
 
 export default constants
--- src/config/constants/types.d.ts
+++ src/config/constants/types.d.ts
@@ -1,7 +1,10 @@
 export interface IConstants {
     APP_NAME: string
-    REDUX_PERSIST_KEY: string
+    DECRED_WALLET_ADDRESS: 'DsamJpphLB5rux2pDwqEw2FJHa4KuSXAnq1'
+    AUTHOR: 'Armando Assunção'
     HTTP_HEADERS: {
         ACCEPT: string
     }
+    REDUX_PERSIST_KEY: string
+    REPO_URL: 'https://www.github.com/ArmandoAssuncao/decred_tickets'
 }
